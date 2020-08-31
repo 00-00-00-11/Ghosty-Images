1cb504a8bf3fd6b3e7c843a8b893ceaabe431735
RyderAsKing <50543053+RyderAsKing@users.noreply.github.com>
2020-08-31 16:06:35 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "ghostylh_uploads";
-$con = new mysqli($db_host, $db_user, $db_pass, $db_name);
-if ($con->connect_error) {
-    die($con->connect_error);
-}
+  $db_host = "your_host";
+  $db_user = "username";
+  $db_pass = "password";
+  $db_name = "database_name";
+  $con = new mysqli($db_host, $db_user, $db_pass, $db_name);
+  if ($con->connect_error) {
+      die($con->connect_error);
+  }
 ?>
